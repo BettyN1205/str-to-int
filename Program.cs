39a59b22b31487ba4f392d171a24fd4ef8dc5913
Betty CR Niu <122338611+BettyN1205@users.noreply.github.com>
2023-05-09 10:37:27 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringPars
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int square=0;
+        Console.WriteLine("Please enter numbers:");
+        string input = Console.ReadLine();
+        string[] inputStr=input.Split(',');
+        int[] num=new int[inputStr.Length];
+        for(int i=0;i<inputStr.Length;i++){
+            num[i]=Convert.ToInt32(inputStr[i]);
+            //num[i]=int.Parse(inputStr[i]);
+            if(num[i]%2==0){
+                square=num[i]*num[i];
+                Console.WriteLine("{0}^2 = {1}",num[i],square);
+            }
+        }
+    }
+}
+}
+
